e172169ef7b54b2a88109f45918c4be4f6acc60f
Jarek Rettinghouse <jarekr@google.com>
2021-01-12 17:23:09 -0800

    hangman setup

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -1,20 +1,81 @@
 from string import ascii_lowercase
-from words import get_random_word
+from Words import *
+
+minWordLen = 3
+maxWordLen = 5
+
+    # setting number of attempts until loss
+def getNumAttempts():
+    x = -1
+    while True:
+        try:
+            x = int(input("How many attempts? (1-20)"))
+            break
+        except ValueError:
+            print("Not a valid number. Please try again.\n")
+    
+    if x > 20:
+        x = 20
+    elif x < 1:
+        x = 1
+    
+    return x
+    
+    
+def getMinWordLen():
+    x = -1
+    while True:
+        try:
+            msg = "What length of word would you like to attempt? (%-%)".format(minWordLen, maxWordLen)
+            inputStr = input(msg)
+            x = int(inputStr)
+            break
+        except ValueError:
+            print("Not a valid number. Please try again..\n")
+    
+    if x > maxWordLen:
+        x = maxWordLen
+    elif x < 1:
+        x = minWordLen
+    
+    return x
+    
 
 
-def get_num_attempts():
-    pass
-    # get input for number between 1 - 20?
-
-
-def get_min_word_length(minLength):
-    pass
-    # get input and check for valid len between 1-5
-
-
-def printGameState():
+def printGameState(attemptsLeft, wordArr):
     # 
-    print("GameState Goes Here")    
+    print("GameState Goes Here")
+    
+    output = "Word: "
+    for letter in wordArr:
+        output = output + str(letter) + " "
+    output = output + "\n"
+    msg = "You have % attempts left\n" % (attemptsLeft) 
+    
+    output = output + msg
+    
+    print(output)
+
+
+def playAgain():
+    # ask if you would like to play again
+    isValidInput = False
+    exitPrompt = ""
+
+    while isValidInput == False:
+        exitPrompt = input('Would you like to play again? (y/n)')
+        exitPrompt.lower()
+        exitPrompt.trim()
+        
+        if exitPrompt == 'y':
+            # isValidInput = True
+            return True
+        elif exitPrompt == 'n':
+            # isValidInput = True
+            return False
+        else:
+            # isValidInput = False
+            print("\nInvalid Input!\n")
 
 
 def play():
@@ -22,24 +83,19 @@ def play():
     gameOver = False
     
     while gameOver == False:
+        # setup
+        wordLen = getMinWordLen()
+        #word = words.getRandomWord()
+        word = "example"
+        
         # main game loop
         playHangman(word, turns)
     
-        # ask if you would like to play again
-        isValidInput = False
-        exitPrompt = ""
-        while isValidInput == False
-            exitPrompt = input('Would you like to play again? (y/n)')
-            exitPrompt.lower()
-            exitPrompt.trim()
-            
-            if exitPrompt == 'y':
-                pass
-            elif exitPrompt == 'n':
-                gameOver = True
-                # return
-            else:
-                print(Invalid Input!)
+        gameOver = playAgain()
+
+
+def playHangman(word, turns):
+    pass    
 
 
 if __name__ == '__main__':
---
+++ Words.py
@@ -0,0 +1,9 @@
+
+
+    # read file wordlist and choose a random word of length provided
+def getRandomWord():
+    pass
+
+
+if __name__ == "__main__"
+    pass
