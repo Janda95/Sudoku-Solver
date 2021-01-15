30118cd529d3fd5581dca21ebaab4e2a916c7145
Jarek Rettinghouse <jarekr@google.com>
2021-01-15 10:39:08 -0800

    basic gui layout

--- CommonLibraries/TkinterGui/gui_example.py
+++ CommonLibraries/TkinterGui/gui_example.py
@@ -11,19 +11,55 @@ import tkinter as tk
 submit = None
 entry = None
 
+class Application(tk.Frame):
+    
+    def __init__(self, hangman, master):
+        super().__init__(master)
+        self.master = master
+        self.hangman = hangman
+        self.pack()
+        self.create_widgets()
+        
+        
+    def create_widgets(self):
+        # Hello button
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+        
+        # Submit button
+        self.submit = tk.Button(self)
+        self.submit["text"] = "Submit"
+        self.submit["command"]
+        
+        # Quit button
+        self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
+        self.quit.pack(side="bottom")
 
-def submit_str():
-    pass
+
+    def say_hi(self):
+        print("hi there, everyone!")     
+        
+   
+    def submit_guess(self):
+        msg = "Submit: " + ""
+        print(msg)
 
 
 if __name__ == "__main__":
-    top = tk.Tk()
+    '''top = tk.Tk()
     
     label = tk.Text(top, text="Hello").grid(0, 0)
     submit = tk.Button(top, text="World", command = submit_str).grid(1, 1)
     entry = tk.Entry(top).grid(1, 0)
     
     # add widgets
-    
-    
     top.mainloop()
+    '''
+    
+    root = tk.Tk()
+    hangman = None
+    app = Application(root, hangman, root)
+    
+    app.mainloop()
